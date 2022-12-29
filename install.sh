d0fad756397a269842c33ebc040c40334f40cc14
mozhenghua <baisui@qlangtech.com>
2022-12-29 19:13:13 +0800

    upgrade tis version 3.7.0

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-mvn install -DskipRat -DskipTests=true -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client -am \
+mvn install -DskipRat -DskipTests=true -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package -am \
 -DaltDeploymentRepository=base::default::http://localhost:8080/snapshot
