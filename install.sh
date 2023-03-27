fadb3cf902c731119e37ddc6e473e0a889a64336
mozhenghua <baisui@qlangtech.com>
2023-03-27 20:21:32 +0800

    Refactor JdbcInterpreter for DataSourceFactoryPluginStore getter process

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-mvn deploy -DskipRat -DskipTests=true -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package -am \
+mvn install -DskipRat -DskipTests=true -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package -am \
 -DaltDeploymentRepository=base::default::http://localhost:8080/snapshot
