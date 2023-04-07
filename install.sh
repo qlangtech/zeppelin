af4598897bfc4c769b4ded54d21aa2fe701f7703
mozhenghua <baisui@qlangtech.com>
2023-04-07 17:29:56 +0800

    modfiy distribution package name

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-mvn install -Pbuild-distr -DskipRat -DskipTests=true -Dappname=all -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package,tis-zeppelin,zeppelin-distribution,zeppelin-interpreter-shaded -am \
+mvn clean install -Pbuild-distr -DskipRat -DskipTests=true -Dappname=all -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package,tis-zeppelin,zeppelin-distribution,zeppelin-interpreter-shaded -am \
 -DaltDeploymentRepository=base::default::http://localhost:8080/snapshot
