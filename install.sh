eb8d14d422b1f053e06e0b60cb7505110a33bd26
mozhenghua <baisui@qlangtech.com>
2023-06-30 10:25:58 +0800

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-mvn clean install -Pbuild-distr -DskipRat -DskipTests=true -Dappname=all -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package,tis-zeppelin,zeppelin-distribution,zeppelin-interpreter-shaded -am \
--DaltDeploymentRepository=base::default::http://localhost:8080/snapshot
+mvn clean deploy -Pbuild-distr -DskipRat -DskipTests=true -Dappname=all -pl jdbc-tis,elasticsearch-tis,zeppelin-server,zeppelin-client,zeppelin-package,tis-zeppelin,zeppelin-distribution,zeppelin-interpreter-shaded -am \
+-Ptis-repo
--- tis-zeppelin/start_zeppelin_server.sh
+++ tis-zeppelin/start_zeppelin_server.sh
@@ -1,3 +1,4 @@
 export ZEPPELIN_HOME=/Users/mozhenghua/Downloads/zeppelin-0.10.1-bin-all
 
-mvnDebug test -Dtest=TestZeppelinServerLauncher -DskipRat -o
+#mvnDebug test -Dtest=TestZeppelinServerLauncher -DskipRat -o
+mvn test -Dtest=TestZeppelinServerLauncher -DskipRat -o
